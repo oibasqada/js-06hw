9211fc8e397358e3a3ee452e2dc8b8164b8957a0
oibasqada <tumarbekovmax@gmail.com>
2023-03-16 00:17:45 +0600

    hw in process. Have issues with .push method. wtf is going on?

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function ehehe() {
+// function arriva(array) {
+//     const rev = array.reverse()
+//     return rev
+// }
+//
+// console.log(arriva([4,5,6]))
 
+//__________________________________________________________reverse()//
+
+// function cleaner(clean){
+//     const result = clean.filter(item => item > 0)
+//     return result
+// }
+//
+// console.log(cleaner([0, 1, false, 2, undefined, '', 3, null]))
+
+//__________________________________________________________filter()//
+
+// function quiz(array){
+//     const result = array.filter((item, index) => array.indexOf(item) === index );
+//     return result
+// }
+//
+// console.log(quiz([1, 2, 3, 1, 2]))
+
+//__________________________________________________________filter()//
+
+// function conCate(con, con2) {
+//     const result = con.concat(con2)
+//     return result
+// }
+//
+// console.log(conCate(['a', 'b', 'c'], [1, 2, 3]))
+
+//__________________________________________________________concat()//
+
+function join(joint){
+    const result = joint.push()
+    return result
 }
+
+console.log(join([1, 2, 3,]))
